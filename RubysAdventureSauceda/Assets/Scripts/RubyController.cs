e9cac99e168fdbb0e58feeef528042546f5338d7
aaron4pf <aaronsauceda12@gmail.com>
2022-11-03 15:26:12 -0700

    added movement and first tile
    added movement for ruby and my first tile

---
+++ RubysAdventureSauceda/Assets/Scripts/RubyController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // variables for axis inputs
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // movement vector
+        Vector2 position = transform.position;
+
+        // horizontal and vertical
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        transform.position = position;
+    }
+}
